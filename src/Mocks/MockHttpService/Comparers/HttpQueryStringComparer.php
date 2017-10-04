3cd5ba8bc437ba004b56a85362db1ca81c5b1a84
mattermack <charles.mack@mattersight.com>
2017-10-04 16:33:28 -0500

    Pact PHP Specification in 1.1
    Query cases - query\matches with equals in the query value.json

--- src/Mocks/MockHttpService/Comparers/HttpQueryStringComparer.php
+++ src/Mocks/MockHttpService/Comparers/HttpQueryStringComparer.php
@@ -16,11 +16,13 @@ class HttpQueryStringComparer
         if (filter_var($expected, FILTER_VALIDATE_URL)) {
             $expectedQuery = parse_url($expected, PHP_URL_QUERY);
         }
+        $expectedQuery = urldecode($expectedQuery);
 
         $actualQuery = $actual;
         if (filter_var($actual, FILTER_VALIDATE_URL)) {
             $actualQuery = parse_url($actual, PHP_URL_QUERY);
         }
+        $actualQuery = urldecode($actualQuery);
 
         $result = new \PhpPact\Comparers\ComparisonResult(sprintf("has query %s", ($expectedQuery ? $expectedQuery : "null")));
 
@@ -64,7 +66,34 @@ class HttpQueryStringComparer
             return array();
         }
 
-        $chunks = array_chunk(preg_split('/(&|=)/', $query), 2);
+        $result = array();
+
+        $equalCount = substr_count($query, "=");
+        $ampCount   = substr_count($query,"&");
+
+        if ($equalCount > $ampCount) {
+            $newQuery = "";
+            $expectAmp = false;
+
+            for( $i = 0; $i < strlen($query); $i++ ) {
+                if ($query[$i] == "&") {
+                    $expectAmp = false;
+                    $newQuery .= $query[$i];
+                }
+                else if ($query[$i] == "=") {
+                    if (!$expectAmp) {
+                        $expectAmp = true;
+                        $newQuery .= $query[$i];
+                    } else {
+                        $newQuery .= urlencode($query[$i]);
+                    }
+                } else {
+                    $newQuery .= $query[$i];
+                }
+            }
+        }
+
+        $chunks = array_chunk(preg_split('/(&|=)/', $newQuery), 2);
         $result = array_combine(array_column($chunks, 0), array_column($chunks, 1));
 
         return $result;
