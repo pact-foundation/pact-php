2678d631cc593cd2a9c9c38d281eb2a91bcf40ca
mattermack <charles.mack@mattersight.com>
2017-10-19 11:27:08 -0500

    Adding initial matching rules for Specification 2.0

---
+++ src/Matchers/Rules/MatcherRuleTypes.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Matchers\Rules;
+
+
+class MatcherRuleTypes
+{
+    const RULE_TYPE = 'RuleType';
+    const OBJECT_TYPE = 'type';
+    const REGEX_TYPE = 'regex';
+    const REGEX_PATTERN = 'RegExPattern';
+
+    const MIN_COUNT = 'MinCount';
+}
--- src/Matchers/Rules/MatchingRule.php
+++ src/Matchers/Rules/MatchingRule.php
@@ -1,21 +1,75 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: matr06017
- * Date: 10/19/2017
- * Time: 9:36 AM
- */
 
-namespace Mocks\MockHttpService\Models;
+namespace Matchers\Rules;
 
 
 class MatchingRule
 {
-    private $_objectType;
+    private $_jsonPath;
+    private $_type;
     private $_regexPattern;
-    private $_matchType;
     private $_min;
 
+    public function __construct($jsonPath, $options = array())
+    {
+        $this->_jsonPath = $jsonPath;
 
+        if (!isset($options[MatcherRuleTypes::MIN_COUNT]) && !isset($options[MatcherRuleTypes::RULE_TYPE])) {
+            throw new \Exception("Matching Rule options were not set to either a min value or an rule type");
+        }
+
+        if (isset($options[MatcherRuleTypes::MIN_COUNT])) {
+            $this->_min = intval(MatcherRuleTypes::MIN_COUNT);
+        }
+
+        if ($options[MatcherRuleTypes::RULE_TYPE] == MatcherRuleTypes::REGEX_TYPE && isset($options[MatcherRuleTypes::REGEX_PATTERN])) {
+            $this->_type = MatcherRuleTypes::REGEX_TYPE;
+            $this->_regexPattern = $options[MatcherRuleTypes::REGEX_PATTERN];
+        }
+        if ($options[MatcherRuleTypes::RULE_TYPE] == MatcherRuleTypes::OBJECT_TYPE) {
+            $this->_type = MatcherRuleTypes::OBJECT_TYPE;
+        }
+        else {
+            throw new \Exception("Invalid rule type and options set: " . $options[MatcherRuleTypes::RULE_TYPE] );
+        }
+    }
+
+    /**
+     * Return the type of matching rule: regex | type
+     *
+     * @return string|false
+     */
+    public function getType()
+    {
+        return isset($this->_type) ? $this->_type : false;
+    }
+
+    /**
+     * Get the integer value for the number of matches
+     *
+     * @return int|false
+     */
+    public function getMin()
+    {
+        return isset($this->_min) ? $this->_min : false;
+    }
+
+    /**
+     * Get the JSON path to query
+     *
+     * @return string
+     */
+    public function getJsonPath()
+    {
+        return $this->_jsonPath;
+    }
+
+    /**
+     * @return string|mixed
+     */
+    public function getRegexPattern()
+    {
+        return isset($this->_regexPattern) ? $this->_regexPattern : false;
+    }
 
 }
