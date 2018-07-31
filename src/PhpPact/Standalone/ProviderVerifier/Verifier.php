40eadbde35d56c54c048e7944c73a85cd6bc2cd5
Mattermack <charles.mack@mattersight.com>
2018-07-31 11:23:06 -0500

    debugging Travis CI after being unable to replicate issue locally - provider - add some debug lines

--- src/PhpPact/Standalone/ProviderVerifier/Verifier.php
+++ src/PhpPact/Standalone/ProviderVerifier/Verifier.php
@@ -213,8 +213,8 @@ class Verifier
 
         $process = new Process($arguments, null, null, null, $this->processTimeout);
         $process->setIdleTimeout($this->processIdleTimeout);
-
-        $this->console->write("Verifying PACT with script {$process->getCommandLine()}");
+        $cmd = $process->getCommandLine();
+        $this->console->write("Verifying PACT with script:\n{$cmd}\n\n");
 
         $process->mustRun(function ($type, $buffer) {
             $this->console->write("{$type} > {$buffer}");
