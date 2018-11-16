6361bae3ca0b6608cb363f57c0fe7eb427784bf7
cfmack <cfmack@gmail.com>
2018-11-15 21:22:54 -0600

    modified rmdir based on Symphony

--- src/PhpPact/Standalone/Installer/InstallManager.php
+++ src/PhpPact/Standalone/Installer/InstallManager.php
@@ -79,7 +79,69 @@ class InstallManager
      */
     public static function uninstall()
     {
-        yield \Amp\File\rmdir(self::$destinationDir . DIRECTORY_SEPARATOR . 'pact');
+        $pactInstallPath = self::$destinationDir . DIRECTORY_SEPARATOR . 'pact';
+        if (\file_exists($pactInstallPath)) {
+            self::rmdir($pactInstallPath);
+        }
+    }
+
+    /**
+     * Modified copy of Symphony filesystem remove
+     *
+     *
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is furnished
+        to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+        THE SOFTWARE.
+
+     * @param unknown $files
+     *
+     * @throws IOException
+     */
+    public static function rmdir($files)
+    {
+        if ($files instanceof \Traversable) {
+            $files = \iterator_to_array($files, false);
+        } elseif (!\is_array($files)) {
+            $files = [$files];
+        }
+        $files = \array_reverse($files);
+        foreach ($files as $file) {
+            if (\is_link($file)) {
+                // See https://bugs.php.net/52176
+                \unlink($file);
+
+                if ('\\' !== \DIRECTORY_SEPARATOR || \file_exists($file)) {
+                    throw new \Exception(\sprintf('Failed to remove symlink "%s"', $file));
+                }
+            } elseif (\is_dir($file)) {
+                self::rmdir(new \FilesystemIterator($file, \FilesystemIterator::CURRENT_AS_PATHNAME | \FilesystemIterator::SKIP_DOTS));
+                \rmdir($file);
+
+                if (\file_exists($file)) {
+                    throw new \Exception(\sprintf('Failed to remove directory "%s"', $file));
+                }
+            } else {
+                \unlink($file);
+
+                if (\file_exists($file)) {
+                    throw new \Exception(\sprintf('Failed to remove file "%s"', $file));
+                }
+            }
+        }
     }
 
     /**
