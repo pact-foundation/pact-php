41d5927314f0e5cd9c769debb4aa87d21db762bc
siad007 <siad.ardroumli@gmail.com>
2020-11-26 17:17:28 +0100

    fixed slim 4 usage

--- example/src/Provider/public/index.php
+++ example/src/Provider/public/index.php
@@ -1,26 +1,23 @@
 <?php
 
-use Slim\Http\Request;
-use Slim\Http\Response;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../../../../vendor/autoload.php';
 
-$app = new \Slim\App();
+$app = AppFactory::create();
 
 $app->get('/hello/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
-
-    return $response
-            ->withJson(['message' => "Hello, {$name}"])
-            ->withHeader('Content-Type', 'application/json');
+    $response->getBody()->write(json_encode(['message' => "Hello, {$name}"]));
+    return $response->withHeader('Content-Type', 'application/json');
 });
 
 $app->get('/goodbye/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
-
-    return $response
-        ->withJson(['message' => "Goodbye, {$name}"])
-        ->withHeader('Content-Type', 'application/json');
+    $response->getBody()->write(json_encode(['message' => "Goodbye, {$name}"]));
+    return $response->withHeader('Content-Type', 'application/json');
 });
 
 $app->run();
