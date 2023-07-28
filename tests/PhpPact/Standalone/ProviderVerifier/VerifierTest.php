25778072112236fd0c2b6858b8eaaf5a977b567c
tienvx <tien.xuan.vo@gmail.com>
2023-07-29 01:18:02 +0700

    chore: Disable process timeout

--- tests/PhpPact/Standalone/ProviderVerifier/VerifierTest.php
+++ tests/PhpPact/Standalone/ProviderVerifier/VerifierTest.php
@@ -19,7 +19,7 @@ class VerifierTest extends TestCase
     {
         $publicPath    =  __DIR__ . '/../../../_public/';
 
-        $this->process = new Process(['php', '-S', '127.0.0.1:7202', '-t', $publicPath]);
+        $this->process = new Process(['php', '-S', '127.0.0.1:7202', '-t', $publicPath], null, null, null, null);
 
         $this->process->start();
         $this->process->waitUntil(fn () => is_resource(@fsockopen('127.0.0.1', 7202)));
