c32fc8208cec1d6c37bc78eacda5baac55d67760
tien.xuan.vo <tien.xuan.vo@gmail.com>
2023-10-22 15:00:50 +0700

    chore: Add matchers example

---
+++ example/matchers/consumer/src/Service/HttpClientService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MatchersConsumer\Service;
+
+use GuzzleHttp\Client;
+
+class HttpClientService
+{
+    private Client $httpClient;
+
+    private string $baseUri;
+
+    public function __construct(string $baseUri)
+    {
+        $this->httpClient = new Client();
+        $this->baseUri    = $baseUri;
+    }
+
+    public function getMatchers(): array
+    {
+        $response = $this->httpClient->get("{$this->baseUri}/matchers", [
+            'headers' => ['Accept' => 'application/json'],
+            'query' => ['ignore' => 'statusCode'],
+        ]);
+
+        return \json_decode($response->getBody(), true, 512, JSON_THROW_ON_ERROR);
+    }
+}
