152eceb815601fc9fd1604ef7b22b7f0e18e7ae1
tien.xuan.vo <tien.xuan.vo@gmail.com>
2024-02-25 16:46:00 +0700

    refactor: Remove $this return value

--- src/PhpPact/Consumer/Driver/InteractionPart/RequestDriver.php
+++ src/PhpPact/Consumer/Driver/InteractionPart/RequestDriver.php
@@ -9,29 +9,24 @@ class RequestDriver extends AbstractInteractionPartDriver implements RequestDriv
 {
     public function registerRequest(Interaction $interaction): void
     {
-        $this
-            ->withRequest($interaction)
-            ->withQueryParameters($interaction)
-            ->withHeaders($interaction, InteractionPart::REQUEST)
-            ->withBody($interaction, InteractionPart::REQUEST);
+        $this->withBody($interaction, InteractionPart::REQUEST);
+        $this->withHeaders($interaction, InteractionPart::REQUEST);
+        $this->withQueryParameters($interaction);
+        $this->withRequest($interaction);
     }
 
-    private function withQueryParameters(Interaction $interaction): self
+    private function withQueryParameters(Interaction $interaction): void
     {
         foreach ($interaction->getRequest()->getQuery() as $key => $values) {
             foreach (array_values($values) as $index => $value) {
                 $this->client->call('pactffi_with_query_parameter_v2', $interaction->getHandle(), (string) $key, (int) $index, (string) $value);
             }
         }
-
-        return $this;
     }
 
-    private function withRequest(Interaction $interaction): self
+    private function withRequest(Interaction $interaction): void
     {
         $request = $interaction->getRequest();
         $this->client->call('pactffi_with_request', $interaction->getHandle(), $request->getMethod(), $request->getPath());
-
-        return $this;
     }
 }
