7476d675dbfb44854aa20a32b44decd9baedeaf1
tien.xuan.vo <tien.xuan.vo@gmail.com>
2024-03-05 11:24:34 +0700

    test: Move reusable test method into trait

--- tests/PhpPact/Plugins/Csv/Factory/CsvInteractionDriverFactoryTest.php
+++ tests/PhpPact/Plugins/Csv/Factory/CsvInteractionDriverFactoryTest.php
@@ -2,17 +2,23 @@
 
 namespace PhpPactTest\Plugins\Csv\Factory;
 
-use PhpPact\Consumer\Driver\Body\InteractionBodyDriverInterface;
+use PhpPact\Consumer\Driver\Body\InteractionBodyDriver;
 use PhpPact\Consumer\Driver\Enum\InteractionPart;
 use PhpPact\Consumer\Driver\Interaction\InteractionDriverInterface;
 use PhpPact\Consumer\Driver\InteractionPart\AbstractInteractionPartDriver;
+use PhpPact\Consumer\Driver\InteractionPart\RequestDriver;
 use PhpPact\Consumer\Driver\InteractionPart\RequestDriverInterface;
+use PhpPact\Consumer\Driver\InteractionPart\ResponseDriver;
 use PhpPact\Consumer\Driver\InteractionPart\ResponseDriverInterface;
 use PhpPact\Consumer\Factory\InteractionDriverFactoryInterface;
+use PhpPact\Consumer\Service\MockServer;
+use PhpPact\FFI\Client;
 use PhpPact\Plugins\Csv\Driver\Body\CsvBodyDriver;
+use PhpPact\Plugins\Csv\Driver\Pact\CsvPactDriver;
 use PhpPact\Plugins\Csv\Exception\MissingPluginPartsException;
 use PhpPact\Plugins\Csv\Factory\CsvInteractionDriverFactory;
 use PhpPact\Standalone\MockService\MockServerConfigInterface;
+use PhpPactTest\Helper\FactoryTrait;
 use PHPUnit\Framework\Attributes\TestWith;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
@@ -20,6 +26,8 @@ use ReflectionProperty;
 
 class CsvInteractionDriverFactoryTest extends TestCase
 {
+    use FactoryTrait;
+
     private InteractionDriverFactoryInterface $factory;
     private MockServerConfigInterface|MockObject $config;
 
@@ -39,18 +47,23 @@ class CsvInteractionDriverFactoryTest extends TestCase
     {
         $this->factory = new CsvInteractionDriverFactory(...$pluginParts);
         $driver = $this->factory->create($this->config);
-        $requestBodyDriver = $this->getBodyDriver($this->getRequestDriver($driver));
-        if (in_array(InteractionPart::REQUEST, $pluginParts)) {
-            $this->assertCsvBodyDriver($requestBodyDriver);
-        } else {
-            $this->assertNotCsvBodyDriver($requestBodyDriver);
-        }
-        $responseBodyDriver = $this->getBodyDriver($this->getResponseDriver($driver));
-        if (in_array(InteractionPart::RESPONSE, $pluginParts)) {
-            $this->assertCsvBodyDriver($responseBodyDriver);
-        } else {
-            $this->assertNotCsvBodyDriver($responseBodyDriver);
-        }
+        $this->assertPropertiesInstanceOf($driver, null, [
+            'client' => Client::class,
+            'mockServer' => MockServer::class,
+            'pactDriver' => CsvPactDriver::class,
+            'requestDriver' => RequestDriver::class,
+            'responseDriver' => ResponseDriver::class,
+        ]);
+        $requestDriver = $this->getRequestDriver($driver);
+        $this->assertPropertiesInstanceOf($requestDriver, AbstractInteractionPartDriver::class, [
+            'client' => Client::class,
+            'bodyDriver' => in_array(InteractionPart::REQUEST, $pluginParts) ? CsvBodyDriver::class : InteractionBodyDriver::class,
+        ]);
+        $responseDriver = $this->getResponseDriver($driver);
+        $this->assertPropertiesInstanceOf($responseDriver, AbstractInteractionPartDriver::class, [
+            'client' => Client::class,
+            'bodyDriver' => in_array(InteractionPart::RESPONSE, $pluginParts) ? CsvBodyDriver::class : InteractionBodyDriver::class,
+        ]);
     }
 
     public function testMissingPluginPartsException(): void
@@ -73,22 +86,5 @@ class CsvInteractionDriverFactoryTest extends TestCase
         $reflection = new ReflectionProperty($driver, 'responseDriver');
 
         return $reflection->getValue($driver);
-    }
-
-    private function assertCsvBodyDriver(InteractionBodyDriverInterface $bodyDriver): void
-    {
-        $this->assertInstanceOf(CsvBodyDriver::class, $bodyDriver);
-    }
-
-    private function assertNotCsvBodyDriver(InteractionBodyDriverInterface $bodyDriver): void
-    {
-        $this->assertNotInstanceOf(CsvBodyDriver::class, $bodyDriver);
-    }
-
-    private function getBodyDriver(RequestDriverInterface|ResponseDriverInterface $interactionPartDriver): InteractionBodyDriverInterface
-    {
-        $reflection = new ReflectionProperty(AbstractInteractionPartDriver::class, 'bodyDriver');
-
-        return $reflection->getValue($interactionPartDriver);
     }
 }
