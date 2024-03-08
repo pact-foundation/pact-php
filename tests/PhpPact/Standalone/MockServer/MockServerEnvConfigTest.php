abd85056332922869c107ab22dfa5effd1a86f25
tien.xuan.vo <tien.xuan.vo@gmail.com>
2024-03-08 10:07:47 +0700

    test: Preserve env while testing MockServerEnvConfig

--- tests/PhpPact/Standalone/MockServer/MockServerEnvConfigTest.php
+++ tests/PhpPact/Standalone/MockServer/MockServerEnvConfigTest.php
@@ -13,80 +13,96 @@ class MockServerEnvConfigTest extends TestCase
     #[TestWith(['PACT_MOCK_SERVER_HOST=example.test', 'example.test'])]
     public function testHost(string $assignment, string $host): void
     {
-        putenv($assignment);
-        $config = new MockServerEnvConfig();
-        static::assertSame($host, $config->getHost());
+        $this->preserveEnv('PACT_MOCK_SERVER_PORT', $assignment, function () use ($host) {
+            $config = new MockServerEnvConfig();
+            $this->assertSame($host, $config->getHost());
+        });
     }
 
     #[TestWith(['PACT_MOCK_SERVER_PORT', 0])]
     #[TestWith(['PACT_MOCK_SERVER_PORT=123', 123])]
     public function testPort(string $assignment, int $port): void
     {
-        putenv($assignment);
-        $config = new MockServerEnvConfig();
-        static::assertSame($port, $config->getPort());
+        $this->preserveEnv('PACT_MOCK_SERVER_PORT', $assignment, function () use ($port) {
+            $config = new MockServerEnvConfig();
+            $this->assertSame($port, $config->getPort());
+        });
     }
 
     #[TestWith(['PACT_CONSUMER_NAME', null])]
     #[TestWith(['PACT_CONSUMER_NAME=consumer', 'consumer'])]
     public function testConsumer(string $assignment, ?string $consumer): void
     {
-        putenv($assignment);
-        if (!$consumer) {
-            $this->expectException(MissingEnvVariableException::class);
-            $this->expectExceptionMessage('Please provide required environmental variable PACT_CONSUMER_NAME!');
-        }
-        $config = new MockServerEnvConfig();
-        static::assertSame($consumer, $config->getConsumer());
+        $this->preserveEnv('PACT_CONSUMER_NAME', $assignment, function () use ($consumer) {
+            if (!$consumer) {
+                $this->expectException(MissingEnvVariableException::class);
+                $this->expectExceptionMessage('Please provide required environmental variable PACT_CONSUMER_NAME!');
+            }
+            $config = new MockServerEnvConfig();
+            $this->assertSame($consumer, $config->getConsumer());
+        });
     }
 
     #[TestWith(['PACT_PROVIDER_NAME', null])]
     #[TestWith(['PACT_PROVIDER_NAME=provider', 'provider'])]
     public function testProvider(string $assignment, ?string $provider): void
     {
-        putenv($assignment);
-        if (!$provider) {
-            $this->expectException(MissingEnvVariableException::class);
-            $this->expectExceptionMessage('Please provide required environmental variable PACT_PROVIDER_NAME!');
-        }
-        $config = new MockServerEnvConfig();
-        static::assertSame($provider, $config->getProvider());
+        $this->preserveEnv('PACT_PROVIDER_NAME', $assignment, function () use ($provider) {
+            if (!$provider) {
+                $this->expectException(MissingEnvVariableException::class);
+                $this->expectExceptionMessage('Please provide required environmental variable PACT_PROVIDER_NAME!');
+            }
+            $config = new MockServerEnvConfig();
+            $this->assertSame($provider, $config->getProvider());
+        });
     }
 
     #[TestWith(['PACT_OUTPUT_DIR', null])]
     #[TestWith(['PACT_OUTPUT_DIR=/path/to/pact/dir', '/path/to/pact/dir'])]
     public function testPactDir(string $assignment, ?string $pactDir): void
     {
-        $pactDir ??= \sys_get_temp_dir();
-        putenv($assignment);
-        $config = new MockServerEnvConfig();
-        static::assertSame($pactDir, $config->getPactDir());
+        $this->preserveEnv('PACT_OUTPUT_DIR', $assignment, function () use ($pactDir) {
+            $pactDir ??= \sys_get_temp_dir();
+            $config = new MockServerEnvConfig();
+            $this->assertSame($pactDir, $config->getPactDir());
+        });
     }
 
     #[TestWith(['PACT_LOG', null])]
     #[TestWith(['PACT_LOG=/path/to/log/dir', '/path/to/log/dir'])]
     public function testLog(string $assignment, ?string $logDir): void
     {
-        putenv($assignment);
-        $config = new MockServerEnvConfig();
-        static::assertSame($logDir, $config->getLog());
+        $this->preserveEnv('PACT_LOG', $assignment, function () use ($logDir) {
+            $config = new MockServerEnvConfig();
+            $this->assertSame($logDir, $config->getLog());
+        });
     }
 
     #[TestWith(['PACT_LOGLEVEL', null])]
     #[TestWith(['PACT_LOGLEVEL=trace', 'TRACE'])]
     public function testLogLevel(string $assignment, ?string $logLevel): void
     {
-        putenv($assignment);
-        $config = new MockServerEnvConfig();
-        static::assertSame($logLevel, $config->getLogLevel());
+        $this->preserveEnv('PACT_LOGLEVEL', $assignment, function () use ($logLevel) {
+            $config = new MockServerEnvConfig();
+            $this->assertSame($logLevel, $config->getLogLevel());
+        });
     }
 
     #[TestWith(['PACT_SPECIFICATION_VERSION', '3.0.0'])]
     #[TestWith(['PACT_SPECIFICATION_VERSION=1.1.0', '1.1.0'])]
     public function testPactSpecificationVersion(string $assignment, ?string $specificationVersion): void
     {
+        $this->preserveEnv('PACT_SPECIFICATION_VERSION', $assignment, function () use ($specificationVersion) {
+            $config = new MockServerEnvConfig();
+            $this->assertSame($specificationVersion, $config->getPactSpecificationVersion());
+        });
+    }
+
+    private function preserveEnv(string $env, string $assignment, callable $callback): void
+    {
+        $value = getenv($env);
         putenv($assignment);
-        $config = new MockServerEnvConfig();
-        static::assertSame($specificationVersion, $config->getPactSpecificationVersion());
+        $callback();
+        putenv("$env=$value");
     }
 }
